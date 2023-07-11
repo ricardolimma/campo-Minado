d71b410908c9d9749df5e68c16f0f321720d4fc9
ricardo <ricardolima5722@gmail.com>
2023-07-11 19:04:51 -0300

    Classe Tabuleiro.

--- campoMinado/src/br/com/cod3r/cm/modelo/Campo.java
+++ campoMinado/src/br/com/cod3r/cm/modelo/Campo.java
@@ -75,6 +75,10 @@ public class Campo {
 		minado = true;
 	}
 	
+	public boolean isMinado() {
+		return minado;
+	}
+	
 	public boolean isMarcado() {
 		return marcado;
 	}
@@ -85,6 +89,44 @@ public class Campo {
 	
 	public boolean isFechado() {
 		return !isAberto();
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+	
+	boolean objetivoAlcancado() {
+		boolean desvendado = !minado && aberto;
+		boolean protegido = minado && marcado;
+		return desvendado || protegido;
+	}
+	
+	long minasNaVizinhaca() {
+		return vizinhos.stream().filter(v -> v.minado).count();
+	}
+	
+	void reiniciar() {
+		aberto = false;
+		minado = false;
+		marcado = false;
+	}
+	
+	public String toString() {
+		if(marcado) {
+			return "x";
+		} else if(aberto && minado) {
+			return "*";
+		} else if(aberto && minasNaVizinhaca() > 0) {
+			return Long.toString(minasNaVizinhaca());
+		} else if(aberto) {
+			return " ";
+		} else {
+			return "?";
+		}
 	}
 
 }
