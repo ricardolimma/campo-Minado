85587e999a503e50c64f1148359217d952744dcc
Ricardo <ricardolima5722@gmail.com>
2023-07-22 13:23:13 -0300

    implementações da interface gráfica do jogo.

--- campoMinado/src/br/com/cod3r/cm/modelo/Campo.java
+++ campoMinado/src/br/com/cod3r/cm/modelo/Campo.java
@@ -50,7 +50,7 @@ public class Campo {
 		}	
 	}
 	
-	void alternarMarcacao() {
+	public void alternarMarcacao() {
 		if(!aberto) {
 			marcado = !marcado;
 			
@@ -62,7 +62,7 @@ public class Campo {
 		}
 	}
 	
-	boolean abrir() {
+	public boolean abrir() {
 		
 		if (!aberto && !marcado) {
 			if(minado) {
@@ -82,7 +82,7 @@ public class Campo {
 		}
 	}
 	
-	boolean vizinhancaSegura() {
+	public boolean vizinhancaSegura() {
 		return vizinhos.stream().noneMatch(v -> v.minado);
 	}
 	
@@ -99,7 +99,10 @@ public class Campo {
 	}
 	
 	void setAberto(boolean aberto) {
-		notificarObservadores(CampoEvento.ABRIR);
+		this.aberto = aberto;
+		if(aberto) {
+			notificarObservadores(CampoEvento.ABRIR);			
+		}
 	}
 
 	public boolean isAberto() {
@@ -124,8 +127,8 @@ public class Campo {
 		return desvendado || protegido;
 	}
 	
-	long minasNaVizinhaca() {
-		return vizinhos.stream().filter(v -> v.minado).count();
+	public int minasNaVizinhaca() {
+		return (int) vizinhos.stream().filter(v -> v.minado).count();
 	}
 	
 	void reiniciar() {
--- campoMinado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
+++ campoMinado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -2,15 +2,17 @@ package br.com.cod3r.cm.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
-public class Tabuleiro {
+public class Tabuleiro implements CampoObservador {
 	
-	private int linhas;
-	private int colunas;
-	private int minas;
+	private final int linhas;
+	private final int colunas;
+	private final int minas;
 	
    private final List<Campo> campos = new ArrayList<>();
+   private final List<Consumer<ResultadoEvento>> observadores = new ArrayList<>();
    
 	public Tabuleiro(int linhas, int colunas, int minas) {
 		this.linhas = linhas;
@@ -23,17 +25,38 @@ public class Tabuleiro {
 		sortearMinas();
 	}
 	
+	public void paraCadaCampo(Consumer<Campo> funcao) {
+		campos.forEach(funcao);
+	}
+	
+	public int getLinhas() {
+		return linhas;
+	}
+
+
+	public int getColunas() {
+		return colunas;
+	}
+
+
+
+	public void registrarObservador(Consumer<ResultadoEvento> observador) {
+		observadores.add(observador);
+	}
+	
+	private void notificarObservadores(Boolean resultado) {
+		observadores.stream()
+		   .forEach(o -> o.accept(new ResultadoEvento(resultado)));
+	}
+	
 	public void abrir(int linha, int coluna) {
-		try {
+		
 			campos.parallelStream()
 			   .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
 			   .findFirst()
 			   .ifPresent(c -> c.abrir());
-		} catch (Exception e) {
-			campos.forEach(c -> c.setAberto(true));
-			throw e;
-		}
 	}
+	
 	
 
 	public void alterarMarcacao(int linha, int coluna) {
@@ -47,8 +70,10 @@ public class Tabuleiro {
 	private void gerarCampos() {
 		for (int linha = 0; linha < linhas; linha++) {
 			for(int coluna = 0; coluna < colunas; coluna++) {
+				Campo campo = new Campo(linha, coluna);
+				campo.registrarObservadores(this);
+				campos.add(campo);
 				
-				campos.add(new Campo(linha, coluna));
 				
 			}
 		}	
@@ -80,7 +105,24 @@ public class Tabuleiro {
 	public void reiniciar() {
 		campos.stream().forEach(c -> c.reiniciar());
 		sortearMinas();
-
+	}
+	
+	@Override
+	public void eventoOcorreu(Campo campo, CampoEvento evento) {
+		if(evento == CampoEvento.EXPLODIR) {
+			mostrarMinas();
+			notificarObservadores(false);
+			
+		} else if(objetivoAlcancado()) {
+			notificarObservadores(true);
+		}
 	}
 
+	private void mostrarMinas() {
+		campos.stream()
+		.filter(c -> c.isMinado())
+		.filter(c -> !c.isMarcado())
+		.forEach(c -> c.setAberto(true));
+		
+	}
 }
